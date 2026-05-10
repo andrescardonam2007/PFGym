b9b338a53352255018d52675471f53372e427e75
Samuel-Gomez19 <fumigacion2005@gmail.com>
2026-05-09 21:14:26 -0500

    .

---
+++ Gym.Domain/Enums/ClassStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gym.Domain.Enums
+{
+    public enum ClassStatus
+    {
+        Libre=0,
+        Lleno= 1
+    }
+}
